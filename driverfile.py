b61e065b9ed94e4106da070c54d2faf230f54d7c
gowthamsankar43 <gowthamsankar43@outlook.com>
2024-09-24 14:32:21 +0530

    Created spark data frame with OLAP data

--- driverfile.py
+++ driverfile.py
@@ -1,8 +1,11 @@
+import os
 import sys
+from logging.config import fileConfig
 
 import get_env_variables as evar
 from spark_session import get_spark_object
 from validate_spark import get_current_date
+from extractfiles import extractfiles, display_df
 import logging
 import logging.config
 
@@ -10,12 +13,54 @@ logging.config.fileConfig('properties/configuration/logging.config')
 
 
 def main():
+    global olap_file, oltp_file, file_format, inferSchema, header
     try:
         logging.info('Initializing Spark context')
         spark = get_spark_object(evar.environment, evar.applicationName)
 
         logging.info(' Validating Spark context')
         get_current_date(spark)
+
+        olap_file_dir = []
+        oltp_file_dir = []
+
+        for oltp_file in os.listdir(evar.src_oltp):
+            oltp_file_dir.append(evar.src_oltp + '\\' + oltp_file)
+
+        for olap_file in os.listdir(evar.src_olap):
+            olap_file_dir.append(evar.src_olap + '\\' + olap_file)
+
+            if olap_file.endswith('.parquet'):
+                file_format = 'parquet'
+                header = 'NA'
+                inferSchema = 'NA'
+            elif olap_file.endswith('.csv'):
+                file_format = 'csv'
+                header = evar.header
+                inferSchema = evar.inferSchema
+            elif olap_file.endswith('.json'):
+                file_format = 'json'
+                header = None
+                inferSchema = None
+
+        print(olap_file_dir)
+
+        df_users = extractfiles(spark=spark, file_dir=olap_file_dir[3], file_format=file_format, header=header,
+                                inferSchema=inferSchema)
+        df_gamesmetadata = extractfiles(spark=spark, file_dir=olap_file_dir[1], file_format=file_format, header=header,
+                                        inferSchema=inferSchema)
+        df_games = extractfiles(spark=spark, file_dir=olap_file_dir[0], file_format=file_format, header=header,
+                                inferSchema=inferSchema)
+        df_recommendations = extractfiles(spark=spark, file_dir=olap_file_dir[2], file_format=file_format,
+                                          header=header,
+                                          inferSchema=inferSchema)
+
+        logging.info("Displaying the data frame")
+        display_df(df_users)
+        display_df(df_gamesmetadata)
+        display_df(df_games)
+        display_df(df_recommendations)
+
     except Exception as e:
         logging.error("An error occurred when calling main() please check the trace ==", str(e))
         sys.exit(1)
---
+++ extractfiles.py
@@ -0,0 +1,33 @@
+import logging.config
+
+logging.config.fileConfig('properties/configuration/logging.config')
+
+logger = logging.getLogger('Extractfile')
+
+
+def extractfiles(spark, file_dir, file_format, header, inferSchema):
+    global olap_file_dir, oltp_file_dir
+    try:
+        logger.warning("File extraction started ....")
+        if file_format == 'parquet':
+            df = spark.read.format(file_format).load(file_dir)
+        elif file_format == 'csv':
+            df = spark.read.format(file_format).option('header', header).option('inferSchema', inferSchema).load(
+                file_dir)
+        elif file_format == 'json':
+            df = spark.read.format(file_format).load(file_dir)
+
+    except Exception as e:
+        logger.error("An error occured at extractfiles", e)
+        raise
+
+    else:
+        logger.warning("Dataframe is created successfully is of {}".format(file_format))
+
+    return df
+
+
+def display_df(df):
+    df_show = df.show()
+
+    return df_show
